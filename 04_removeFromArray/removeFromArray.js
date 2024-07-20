0e82413ea22a8b0d3f17334c0b4fc5f2b3c1761b
subsanddubs11 <subsanddubs11@tutanota.com>
2024-07-19 17:17:12 -0700

    Fixed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, value, ...inputs) {
+  let filteredArray = arr.filter((el) => el !== value);
+  for (const input of inputs) {
+    filteredArray = filteredArray.filter((el) => el !== input);
+  }
+  return filteredArray;
 };
 
 // Do not edit below this line
