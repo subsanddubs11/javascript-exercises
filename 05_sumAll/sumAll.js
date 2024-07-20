f513bbcd1043a6c1bc0a17ac61846c2d5c0f4af8
subsanddubs11 <subsanddubs11@tutanota.com>
2024-07-19 18:27:30 -0700

    Fixed exercise 8

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 < 0 || num2 < 0 || isNaN(num1) || isNaN(num2) || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+    return 'ERROR';
+  }
 
+  let total = 0;
+  let startNum = 0;
+  let endNum = 0;
+
+  if (num2 < num1) {
+    startNum = num2;
+    endNum = num1;
+  } else {
+    startNum = num1;
+    endNum = num2;
+  }
+
+  for (let i = startNum; i <= endNum; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,80 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(...nums) {
+	let total = 0;
+  let numArray = [];
+
+  for (const num of nums) {
+    if(Array.isArray(num)) {
+      for (let i = 0; i < num.length; i++) {
+        numArray.push(num[i]);
+      }
+    } else {
+      numArray.push(num);
+    }
+  }
+  
+  numArray.forEach((el) => {
+    if (el == '') {
+      el = 0;
+    }
+    total += el;
+  });
+
+  return total;
+}
+
+const multiply = function(...nums) {
+  let total = 1;
+  let numArray = [];
+
+  for (const num of nums) {
+    if(Array.isArray(num)) {
+      for (let i = 0; i < num.length; i++) {
+        numArray.push(num[i]);
+      }
+    } else {
+      numArray.push(num);
+    }
+  }
+  
+  numArray.forEach((el) => {
+    if (el == '') {
+      el = 0;
+    }
+    total *= el;
+  });
+
+  return total;
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+  let total = 1;
+	for (let i = 0; i < num2; i++) {
+    total *= num1;
+  }
+  return total;
 };
 
-const power = function() {
-	
-};
+const factorial = function(num) {
 
-const factorial = function() {
-	
+  let total = 1;
+
+  if(num == 0) {
+    return 1;
+  }
+
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
